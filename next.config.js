abbb32bc2e1c5284df9447975f407f4d7bd86ece
HellGround <dlgudwns783@gmail.com>
2023-05-25 18:38:44 +0900

    dev: twin macro 적용. 방법 - next.config.js에서 웹팩 설정

--- next.config.js
+++ next.config.js
@@ -1,11 +1,42 @@
-const withTwin = require('./config/withTwin.js');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, options) => {
+    const { dev, isServer } = options;
+    config.module.rules.push({
+      test: /\.(tsx|ts)$/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: 'babel-loader',
+          options: {
+            sourceMaps: dev,
+            plugins: [
+              require.resolve('babel-plugin-macros'),
+              [require.resolve('@babel/plugin-syntax-typescript'), { isTSX: true }],
+            ],
+          },
+        },
+      ],
+    });
+
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...(config.resolve.fallback || {}),
+        fs: false,
+        module: false,
+        path: false,
+        os: false,
+        crypto: false,
+      };
+    }
+
+    return config;
+  },
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 module.exports = nextConfig;
-// TODO: withTwin으로적용해보기..지금은 .babelrc 직접사용
-// module.exports = withTwin(nextConfig);
