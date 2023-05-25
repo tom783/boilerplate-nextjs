f4962d27071d9cbea55808a5ff191d19db15230c
HellGround <dlgudwns783@gmail.com>
2023-05-25 12:14:33 +0900

    dev: eslint, prettier 설정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,6 @@
-import React from "react";
-import Document, {
-  DocumentContext,
-  Head,
-  Html,
-  Main,
-  NextScript,
-} from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import React from 'react';
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -15,8 +9,7 @@ export default class MyDocument extends Document {
     try {
       ctx.renderPage = async () => {
         return originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
         });
       };
 
