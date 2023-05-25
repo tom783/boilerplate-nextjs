f42175218a471e1b352b9dc1a32961f213bdb5c5
HellGround <dlgudwns783@gmail.com>
2023-05-25 18:39:57 +0900

    dev: storybook 오류 해결

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,33 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 const config: StorybookConfig = {
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm',
+  ],
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
   docs: {
     autodocs: 'tag',
+  },
+  core: {
+    disableTelemetry: true,
+  },
+  // Twin recommends adding the babel config here as Next.js disables SWC when .babelrc is added to the root.
+  babel: async (options) => {
+    return {
+      ...options,
+      plugins: [
+        ...(options.plugins as any),
+        'babel-plugin-twin',
+        'babel-plugin-macros',
+        'babel-plugin-styled-components',
+      ],
+    };
   },
 };
 export default config;
