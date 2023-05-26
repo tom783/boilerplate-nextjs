6bba135acfff3e8e6f398a2fb273c327f320c5fd
HellGround <dlgudwns783@gmail.com>
2023-05-26 18:56:02 +0900

    dev: 코드정리

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,11 +8,5 @@ const Styles = {
 };
 
 export default function Home() {
-  return (
-    <Styles.Home tw='bg-blue-50'>
-      {[1, 2, 3, 4, 5].map((i, idx) => {
-        return <Button key={idx}>test</Button>;
-      })}
-    </Styles.Home>
-  );
+  return <Styles.Home tw='bg-blue-50'>HOME</Styles.Home>;
 }
